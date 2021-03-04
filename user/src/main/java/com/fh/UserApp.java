fca71bde01e9b8e3d313de7ca866467834f86e1e
zhengyang <zy15039258589@163.com>
2021-03-04 16:59:16 +0800

    修改了配置文件

--- user/src/main/java/com/fh/UserApp.java
+++ user/src/main/java/com/fh/UserApp.java
@@ -2,12 +2,14 @@ package com.fh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
+@EnableDiscoveryClient //声明是Eureka 客户端
 public class UserApp
 {
     public static void main( String[] args )
