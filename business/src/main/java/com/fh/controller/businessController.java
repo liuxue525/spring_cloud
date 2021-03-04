aa4f7b506c3afb292ffbc92802f30539b46490a3
zhengyang <zy15039258589@163.com>
2021-03-04 16:21:13 +0800

    中心

---
+++ business/src/main/java/com/fh/controller/businessController.java
@@ -0,0 +1,11 @@
+package com.fh.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("businessController")
+public class businessController {
+
+
+}
